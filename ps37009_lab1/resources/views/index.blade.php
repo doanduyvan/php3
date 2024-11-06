ce4a97997e5e6f788ea18596bf8b644e4c879bd8
doanduyvan <duyvanlee2001@gmail.com>
2024-11-07 01:46:22 +0700

    update

--- ps37009_lab1/resources/views/index.blade.php
+++ ps37009_lab1/resources/views/index.blade.php
@@ -1,1 +1,6 @@
+
+@extends('layout')
+
+@section('content')
 <h1>Đây là trang chủ</h1>
+@endsection
--- ps37009_lab1/resources/views/lienhe.blade.php
+++ ps37009_lab1/resources/views/lienhe.blade.php
@@ -1,1 +1,6 @@
+
+@extends('layout')
+
+@section('content')
 <h1>Đây là trang liên hệ</h1>
+@endsection
